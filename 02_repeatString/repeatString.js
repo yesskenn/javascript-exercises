755ac626791e589d9792cd325dfb7c9d6010cd9f
Yessi Kenney <yessikenney@gmail.com>
2025-03-31 14:53:14 -0700

    first exercise test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,9 @@
 const repeatString = function() {
+    - Take note of the above function call- how exactly is it being called?
+
+    - Create a variable to hold the string you're going to return, create a loop that repeats the given number of times and add the given string to the result on each loop.
+    
+    - If running `npm test repeatString.spec.js` returns results similar to the below, make sure you have enabled the rest of the tests, as described in the instructions above.
 
 };
 
