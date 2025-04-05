8a37ec384969bbc4d6f7347c3f29e812b4ff39be
Yessi Kenney <yessikenney@gmail.com>
2025-04-05 15:16:43 -0700

    creating variables

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,10 @@
-const reverseString = function() {
+let str = 'hello, there!'
+const reverseString = function(str,space) {
+    const stringArray = str.split(space);
+    const numberArray = stringArray.map(nummber);
+    return numberArray;
+    var guess = ("Enter your guess", "Number from 1 to 9");
+console.log(guess);
 
 };
 
