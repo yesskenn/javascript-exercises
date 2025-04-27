37d671449042006832ff492100e18e8da646fe45
Yessi Kenney <yessikenney@gmail.com>
2025-04-27 12:21:48 -0700

    adding number guessing game

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,12 +1,16 @@
-let str = 'hello, there!'
-const reverseString = function(str,space) {
+/** const reverseString = function(str,space) {
     const stringArray = str.split(space);
     const numberArray = stringArray.map(nummber);
     return numberArray;
     var guess = ("Enter your guess", "Number from 1 to 9");
-console.log(guess);
+console.log(str);
 
-};
+};**/
+
+const a = 5;
+const b = 10;
+const product = a * b;
+console.log('hello');
 
 // Do not edit below this line
-module.exports = reverseString;
+//module.exports = reverseString;
